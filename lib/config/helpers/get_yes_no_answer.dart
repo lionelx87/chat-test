be98f3ad57ba87838f28fa7c9a7e5b5fd1abe243
Lionel Acosta <lionelx87@gmail.com>
2023-06-23 01:24:05 -0300

    feat(app): mappers

--- lib/config/helpers/get_yes_no_answer.dart
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:second_app/domain/entities/message.dart';
+import 'package:second_app/infrastructure/models/yes_no_model.dart';
 
 class GetYesNoAnswer {
 
@@ -7,6 +8,23 @@ class GetYesNoAnswer {
 
   Future<Message> getAnswer() async {
     final response = await _dio.get('https://yesno.wtf/api');
-    throw UnimplementedError();
+
+    final yesNoModel = YesNoModel.fromJsonMap(response.data);
+
+    return yesNoModel.toMessageEntity();
+
+    // return Message(
+    //   text: yesNoModel.answer, 
+    //   sender: Sender.YOU,
+    //   imageUrl: yesNoModel.image
+    // );
+
+    // return Message(
+    //   text: response.data['answer'], 
+    //   imageUrl: response.data['image'],
+    //   sender: Sender.YOU
+    // );
+    
+    // throw UnimplementedError();
   }
 }
